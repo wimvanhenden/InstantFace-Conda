a5f6a4d28b94a2cbc55ccc44c36fd8979f6f8716
Wim Vanhenden <wim.vanhenden@gmail.com>
2024-03-15 15:01:11 +0100

    initial init

---
+++ insight_face_test.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import insightface
+from insightface.app import FaceAnalysis
+from insightface.data import get_image as ins_get_image
+
+#workaround for numpy
+np.int = np.int_
+
+app = FaceAnalysis(name='antelopev2', root="./", providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+app.prepare(ctx_id=0, det_size=(640, 640))
+img = ins_get_image('t1')
+faces = app.get(img)
+rimg = app.draw_on(img, faces)
+cv2.imwrite("./t1_output.jpg", rimg)
